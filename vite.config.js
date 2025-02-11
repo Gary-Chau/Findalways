d5542ffa764a1fc9717e84bc5c1d704dfec27b65
Gary <gary@icshk.local>
2025-02-11 09:41:10 +0800

    added file

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: './',
+  base: '/Findalways/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
